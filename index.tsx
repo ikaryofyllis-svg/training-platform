fa1093ae1cb03cfd51206140a70792355a5dfd91
Iason Karyofyllis <ikaryofyllis@gmail.com>
2026-02-23 13:37:55 +0100

    fix root duplication

--- index.tsx
+++ index.tsx
@@ -24,12 +24,4 @@ const updateSW = registerSW({
   }
 });
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element to mount to");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
